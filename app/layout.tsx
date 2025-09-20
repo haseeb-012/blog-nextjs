8e6ccdb7a976beb1b4d333f72727c6ab1b0cf193
Haseeb Sajjad <haseebsajjad666@gmail.com>
2025-09-20 20:27:41 +0500

    Remove Footer import from layout component for cleaner structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import Header from "@/components/Header";
 import "./globals.css";
-import Footer from "@/components/Footer";
+
 
 export const metadata: Metadata = {
   title: "thetextlist.com",
